24980a8ca69a151ffcbd828e7c9119f1c9077984
Laura Martin <surminus@gmail.com>
2020-02-05 13:35:46 +0000

    Split config into separate file
    Similar to previous commit, this tidies config functions into a separate
    file.

---
+++ config.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+// Config represents all options that can be configured by a flecs config file
+type Config struct {
+	Environment string
+	ClusterName string
+	Pipeline    []Step
+}
+
+// Step describes a step in the pipeline
+type Step struct {
+	Name  string
+	Class string
+}
+
+// LoadConfig will load all configuration options if they exist, allowing
+// environment specific options to override top level options
+func LoadConfig() (config Config, err error) {
+	clusterName := viper.GetString("cluster_name")
+	pipeline := viper.Get("pipeline").([]interface{})
+
+	envConfig := ""
+	if cmdFlagEnvironment != "" {
+		envConfig = envConfigOption("")
+	}
+
+	if envConfig != "" {
+		if viper.IsSet(envConfig) {
+			if viper.IsSet(envConfigOption("cluster_name")) {
+				clusterName = viper.GetString(envConfigOption("cluster_name"))
+			}
+
+			if viper.IsSet(envConfigOption("pipeline")) {
+				pipeline = viper.Get(envConfigOption("pipeline")).([]interface{})
+			}
+		}
+	}
+
+	steps := []Step{}
+
+	for _, step := range pipeline {
+		for key, value := range step.(map[interface{}]interface{}) {
+			class := key.(string)
+			details := value.(map[interface{}]interface{})
+
+			name := details["name"].(string)
+
+			steps = append(steps, Step{
+				Name:  name,
+				Class: class,
+			})
+		}
+	}
+
+	config = Config{
+		ClusterName: clusterName,
+		Environment: cmdFlagEnvironment,
+		Pipeline:    steps,
+	}
+
+	return config, err
+}
+
+// envConfigOption resolves the name of the config option in the environment
+// specific part of the configuration file, using the concept viper uses for
+// searching for subkeys, ie "foo.bar.option".
+//
+// If argument is passed as an empty string, then it returns the plain name of
+// the environment subkey, ie environments.[environment]
+func envConfigOption(option string) (result string) {
+	envConfig := strings.Join([]string{"environments", cmdFlagEnvironment}, ".")
+	if option == "" {
+		return envConfig
+	}
+
+	result = strings.Join([]string{envConfig, option}, ".")
+	return result
+}
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strings"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -10,19 +8,6 @@ import (
 var (
 	cmdFlagEnvironment string
 )
-
-// Config represents all options that can be configured by a flecs config file
-type Config struct {
-	Environment string
-	ClusterName string
-	Pipeline    []Step
-}
-
-// Step describes a step in the pipeline
-type Step struct {
-	Name  string
-	Class string
-}
 
 // Task runs a one-off task in the cluster
 type Task struct{}
@@ -32,70 +17,6 @@ type Script struct{}
 
 // Service will update one or many services and wait for completion
 type Service struct{}
-
-// loadConfig will load all configuration options if they exist, allowing
-// environment specific options to override top level options
-func loadConfig() (config Config, err error) {
-	clusterName := viper.GetString("cluster_name")
-	pipeline := viper.Get("pipeline").([]interface{})
-
-	envConfig := ""
-	if cmdFlagEnvironment != "" {
-		envConfig = envConfigOption("")
-	}
-
-	if envConfig != "" {
-		if viper.IsSet(envConfig) {
-			if viper.IsSet(envConfigOption("cluster_name")) {
-				clusterName = viper.GetString(envConfigOption("cluster_name"))
-			}
-
-			if viper.IsSet(envConfigOption("pipeline")) {
-				pipeline = viper.Get(envConfigOption("pipeline")).([]interface{})
-			}
-		}
-	}
-
-	steps := []Step{}
-
-	for _, step := range pipeline {
-		for key, value := range step.(map[interface{}]interface{}) {
-			class := key.(string)
-			details := value.(map[interface{}]interface{})
-
-			name := details["name"].(string)
-
-			steps = append(steps, Step{
-				Name:  name,
-				Class: class,
-			})
-		}
-	}
-
-	config = Config{
-		ClusterName: clusterName,
-		Environment: cmdFlagEnvironment,
-		Pipeline:    steps,
-	}
-
-	return config, err
-}
-
-// envConfigOption resolves the name of the config option in the environment
-// specific part of the configuration file, using the concept viper uses for
-// searching for subkeys, ie "foo.bar.option".
-//
-// If argument is passed as an empty string, then it returns the plain name of
-// the environment subkey, ie environments.[environment]
-func envConfigOption(option string) (result string) {
-	envConfig := strings.Join([]string{"environments", cmdFlagEnvironment}, ".")
-	if option == "" {
-		return envConfig
-	}
-
-	result = strings.Join([]string{envConfig, option}, ".")
-	return result
-}
 
 func main() {
 	execute()
@@ -121,7 +42,7 @@ var cmd = &cobra.Command{
 		Log.Info("Starting pipeline")
 
 		// Each other function should accept the config type
-		config, err := loadConfig()
+		config, err := LoadConfig()
 		CheckError(err)
 
 		for i, step := range config.Pipeline {
