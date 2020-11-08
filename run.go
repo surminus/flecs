00952c781577561c11437b17c6e882bd3396960a
Laura Martin <surminus@gmail.com>
2020-11-08 15:15:12 +0000

    add service creation to run

--- run.go
+++ run.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+)
+
+// Run runs through the pipeline and performs each task
 func (config Config) Run() (err error) {
 	for _, step := range config.Pipeline {
 		Log.Info("Step: ", step.Type)
@@ -10,9 +15,20 @@ func (config Config) Run() (err error) {
 		case "service":
 			Log.Info("Name: ", step.Service.Name)
 
-			// serviceStep := step.Service
-			// serviceStep.clientECS = ClientECS{}
-			// serviceStep.config = config
+			service, ok := config.Services[step.Service.Service]
+			if !ok {
+				return fmt.Errorf("cannot find service configured called %s", step.Service.Service)
+			}
+
+			service.Name = step.Service.Service
+
+			client := Client{Region: config.Region}
+			serviceName, err := service.Create(client, config)
+			if err != nil {
+				return err
+			}
+
+			Log.Infof("Configured service %s", serviceName)
 
 		case "script":
 			Log.Info("Name: ", step.Script.Name)
