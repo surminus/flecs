2db9fc02849e7024a217885662d325b9aa18c637
Laura Martin <surminus@gmail.com>
2020-11-08 12:27:03 +0000

    Continue configuring service creation

--- service.go
+++ service.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/dchest/uniuri"
 )
 
 // ServiceStep will update a Service
@@ -24,26 +26,48 @@ type Service struct {
 	TargetGroupArn string `yaml:"target_group_arn"`
 }
 
-func (s Service) Create(c ClientECS, cfg Config) (serviceName string, err error) {
-	client := c.Client
+func (s Service) Create(c Client, cfg Config) (serviceName string, err error) {
+	// Set up ECS client
+	clientECS, err := c.ECS()
+	if err != nil {
+		return serviceName, err
+	}
+	client := clientECS.Client
 
-	// Generate name
+	// Set up EC2 client
+	clientEC2, err := c.EC2()
+	if err != nil {
+		return serviceName, err
+	}
+
+	// Configure service name
 	serviceNamePrefix := strings.Join([]string{"flecs", cfg.ProjectName}, "-")
 	if cfg.ProjectName != s.Name {
 		serviceNamePrefix = strings.Join([]string{serviceNamePrefix, s.Name}, "-")
 	}
 
 	// Check if the service already exists
-	serviceName, err = s.checkServiceExists(c, cfg, serviceNamePrefix)
+	serviceName, err = s.checkServiceExists(clientECS, cfg, serviceNamePrefix)
 	if err != nil || serviceName != "" {
 		return serviceName, err
 	}
 
 	// Get security group IDs
+	securityGroupIDs, err := clientEC2.getSecurityGroupIDs(cfg.SecurityGroupNames)
+	if err != nil {
+		return serviceName, err
+	}
 
 	// Get subnet IDs
+	subnetIDs, err := clientEC2.getSubnetIDs(cfg.SubnetNames)
+	if err != nil {
+		return serviceName, err
+	}
 
 	// Register task definition
+
+	// Generate new service name with uuid
+	serviceName = strings.Join([]string{serviceNamePrefix, uniuri.NewLen(8)}, "-")
 
 	// Create service
 	createServiceInput := ecs.CreateServiceInput{
@@ -52,8 +76,8 @@ func (s Service) Create(c ClientECS, cfg Config) (serviceName string, err error)
 		LaunchType:   aws.String(s.LaunchType),
 		NetworkConfiguration: &ecs.NetworkConfiguration{
 			AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
-				SecurityGroups: []*string{},
-				Subnets:        []*string{},
+				SecurityGroups: aws.StringSlice(securityGroupIDs),
+				Subnets:        aws.StringSlice(subnetIDs),
 			},
 		},
 		ServiceName:    aws.String(serviceName),
@@ -63,6 +87,13 @@ func (s Service) Create(c ClientECS, cfg Config) (serviceName string, err error)
 	output, err := client.CreateService(&createServiceInput)
 
 	// Wait for service to become stable
+	err = client.WaitUntilServicesStable(&ecs.DescribeServicesInput{
+		Cluster:  aws.String(cfg.ClusterName),
+		Services: aws.StringSlice([]string{aws.StringValue(output.Service.ServiceArn)}),
+	})
+	if err != nil {
+		return serviceName, err
+	}
 
 	return serviceName, err
 }
@@ -112,4 +143,48 @@ func (s Service) checkServiceExists(c ClientECS, cfg Config, serviceNamePrefix s
 	}
 
 	return serviceName, err
+}
+
+func (c ClientEC2) getSecurityGroupIDs(names []string) (ids []string, err error) {
+	input := ec2.DescribeSecurityGroupsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("group-name"),
+				Values: aws.StringSlice(names),
+			},
+		},
+	}
+
+	result, err := c.Client.DescribeSecurityGroups(&input)
+	if err != nil {
+		return ids, err
+	}
+
+	for _, g := range result.SecurityGroups {
+		ids = append(ids, aws.StringValue(g.GroupId))
+	}
+
+	return ids, err
+}
+
+func (c ClientEC2) getSubnetIDs(names []string) (ids []string, err error) {
+	input := ec2.DescribeSubnetsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("tag:Name"),
+				Values: aws.StringSlice(names),
+			},
+		},
+	}
+
+	result, err := c.Client.DescribeSubnets(&input)
+	if err != nil {
+		return ids, err
+	}
+
+	for _, s := range result.Subnets {
+		ids = append(ids, aws.StringValue(s.SubnetId))
+	}
+
+	return ids, err
 }
